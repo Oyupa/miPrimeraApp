858b805665ebf32dfeec79e7b7dd2a0d7283fd12
Oyupa <61736250+Oyupa@users.noreply.github.com>
2024-09-22 11:01:00 +0200

    Add files via upload

---
+++ src/main/java/io/primerconcurrente/mi_primeraapp/domain/SensorAcceso.java
@@ -0,0 +1,27 @@
+package io.primerconcurrente.mi_primeraapp.domain;
+
+import io.primerconcurrente.mi_primeraapp.model.TipoDeAcceso;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "SensorAccesoes")
+public class SensorAcceso extends Sensor {
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TipoDeAcceso tipoDeAcceso;
+
+    public TipoDeAcceso getTipoDeAcceso() {
+        return tipoDeAcceso;
+    }
+
+    public void setTipoDeAcceso(final TipoDeAcceso tipoDeAcceso) {
+        this.tipoDeAcceso = tipoDeAcceso;
+    }
+
+}
